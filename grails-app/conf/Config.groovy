7fafe04b024716c8e7e45daf4a2f05a92e139c24
djtfmartin <djtfmartin@gmail.com>
2011-11-21 01:27:59 +0000

    update
    git-svn-id: http://ala-alerts.googlecode.com/svn/trunk@24 bf2a471e-1231-f234-9b1b-28eadff096fd

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -93,7 +93,7 @@ environments {
              host = "smtp.gmail.com"
              port = 465
              username = "moyesyside@gmail.com"
-             password = "moyes06:"
+             password = "**************:"
              props = ["mail.smtp.auth":"true",
                       "mail.smtp.socketFactory.port":"465",
                       "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
