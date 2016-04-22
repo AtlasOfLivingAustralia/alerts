718934b5d6bf8400741f1b35ca1936c622c9cc4c
m-r-c <m-r-c@users.noreply.github.com>
2016-04-22 13:49:25 +1000

    removes environment-specific properties from config.groovy - should be in external config file and managed via ansible inventories

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -33,18 +33,18 @@ grails.project.groupId = au.org.ala.postie // change this to alter the default p
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
 grails.mime.types = [
-    all:           '*/*',
-    atom:          'application/atom+xml',
-    css:           'text/css',
-    csv:           'text/csv',
-    form:          'application/x-www-form-urlencoded',
-    html:          ['text/html','application/xhtml+xml'],
-    js:            'text/javascript',
-    json:          ['application/json', 'text/json'],
-    multipartForm: 'multipart/form-data',
-    rss:           'application/rss+xml',
-    text:          'text/plain',
-    xml:           ['text/xml', 'application/xml']
+        all:           '*/*',
+        atom:          'application/atom+xml',
+        css:           'text/css',
+        csv:           'text/csv',
+        form:          'application/x-www-form-urlencoded',
+        html:          ['text/html','application/xhtml+xml'],
+        js:            'text/javascript',
+        json:          ['application/json', 'text/json'],
+        multipartForm: 'multipart/form-data',
+        rss:           'application/rss+xml',
+        text:          'text/plain',
+        xml:           ['text/xml', 'application/xml']
 ]
 
 // URL Mapping Cache Max Size, defaults to 5000
@@ -98,10 +98,10 @@ log4j = {
         environments {
             production {
                 rollingFile name: "postie-prod",
-                    maxFileSize: 104857600,
-                    file: logging_dir + "/alerts.log",
-                    threshold: org.apache.log4j.Level.ERROR,
-                    layout: pattern(conversionPattern: "%d [%c{1}]  %m%n")
+                        maxFileSize: 104857600,
+                        file: logging_dir + "/alerts.log",
+                        threshold: org.apache.log4j.Level.ERROR,
+                        layout: pattern(conversionPattern: "%d [%c{1}]  %m%n")
                 rollingFile name: "stacktrace", maxFileSize: 1024, file: logging_dir + "/alerts-stacktrace.log"
             }
             development{
@@ -115,25 +115,25 @@ log4j = {
     }
 
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-	       'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-	       'org.codehaus.groovy.grails.commons', // core / classloading
-	       'org.codehaus.groovy.grails.plugins', // plugins
-           'org.springframework.jdbc',
-           'org.springframework.transaction',
-           'org.codehaus.groovy',
-           'org.grails',
-           'org.apache',
-           'grails.spring',
-           'grails.util.GrailsUtil',
-           'net.sf.ehcache'
-           'grails.app.service.org.grails.plugin.resource'
-           'grails.app.service'
-           'org.ala'
-           'au.org.ala'
-           'grails.app.service.org.grails.plugin.resource.ResourceTagLib'
+            'org.codehaus.groovy.grails.web.pages', //  GSP
+            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+            'org.codehaus.groovy.grails.web.mapping', // URL mapping
+            'org.codehaus.groovy.grails.commons', // core / classloading
+            'org.codehaus.groovy.grails.plugins', // plugins
+            'org.springframework.jdbc',
+            'org.springframework.transaction',
+            'org.codehaus.groovy',
+            'org.grails',
+            'org.apache',
+            'grails.spring',
+            'grails.util.GrailsUtil',
+            'net.sf.ehcache'
+    'grails.app.service.org.grails.plugin.resource'
+    'grails.app.service'
+    'org.ala'
+    'au.org.ala'
+    'grails.app.service.org.grails.plugin.resource.ResourceTagLib'
 
     debug  'au.org.ala.alerts'
 }
@@ -150,87 +150,32 @@ postie.emailInfoSender = 'atlas-alerts@ala.org.au'
 postie.defaultResourceName = 'Atlas'
 postie.enableEmail = true
 
-security.cas.casServerName = 'https://auth.ala.org.au'
-security.cas.uriFilterPattern = ''
-security.cas.uriExclusionFilterPattern = '/images.*,/css.*,/js.*,/less.*'
-security.cas.authenticateOnlyIfLoggedInPattern = "" // pattern for pages that can optionally display info about the logged-in user
-security.cas.loginUrl = 'https://auth.ala.org.au/cas/login'
-security.cas.logoutUrl = 'https://auth.ala.org.au/cas/logout'
-security.cas.casServerUrlPrefix = 'https://auth.ala.org.au/cas'
-security.cas.bypass = false
-
-appContext = 'alerts'
-ala.baseURL = "http://www.ala.org.au"
-bie.baseURL = "http://bie.ala.org.au"
-bie.searchPath = "/search"
 grails.project.groupId = "au.org.ala" // change this to alter the default package name and Maven publishing destination
-ala.userDetailsURL = 'http://auth.ala.org.au/userdetails/userDetails/getUserListFull'
-
-biocache.baseURL = "http://biocache.ala.org.au"
-spatial.baseURL = "http://spatial.ala.org.au"
-collectory.baseURL = "http://collections.ala.org.au"
-
 
 environments {
     development {
         grails.logging.jul.usebridge = true
-        grails.host = "http://dev.ala.org.au"
-        grails.serverURL = "${grails.host}:8080/${appContext}"
-        security.cas.appServerName = "${grails.host}:8080"
-        security.cas.contextPath = "/${appContext}"
         grails.resources.debug = true // cached-resources plugin - keeps original filenames but adds cache-busting params
         grails {
-          mail {
-            host = "localhost"
-            port = 1025
-            username = postie.emailSender
-          }
+            mail {
+                host = "localhost"
+                port = 1025
+                username = postie.emailSender
+            }
         }
     }
     test {
         grails.logging.jul.usebridge = false
-        grails.host = "alerts-test.ala.org.au"
-        grails.serverURL = "http://alerts-test.ala.org.au"
-        security.cas.appServerName = grails.serverURL
-        security.cas.contextPath = ""
         log4j.appender.'errors.File'="/var/log/tomcat/alerts-stacktrace.log"
     }
     production {
         grails.logging.jul.usebridge = false
-        grails.host = "alerts.ala.org.au"
-        grails.serverURL = "http://${grails.host}"
-        security.cas.appServerName = grails.serverURL
-        security.cas.contextPath = ""
         grails {
-          mail {
-            host = "localhost"
-            port = 25
-            username = postie.emailSender
-          }
-        }
-    }
-}
-
-// Uncomment and edit the following lines to start using Grails encoding & escaping improvements
-
-/* remove this line
-// GSP settings
-grails {
-    views {
-        gsp {
-            encoding = 'UTF-8'
-            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
-            codecs {
-                expression = 'html' // escapes values inside null
-                scriptlet = 'none' // escapes output from scriptlets in GSPs
-                taglib = 'none' // escapes output from taglibs
-                staticparts = 'none' // escapes output from static template parts
+            mail {
+                host = "localhost"
+                port = 25
+                username = postie.emailSender
             }
         }
-        // escapes all not-encoded output at final stage of outputting
-        filteringCodecForContentType {
-            //'text/html' = 'html'
-        }
     }
 }
-remove this line */
