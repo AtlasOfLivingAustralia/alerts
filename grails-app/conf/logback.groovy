1c9f8d073fd5040383fba1b390c1f312fcd0c74d
nickdos <nickdos@gmail.com>
2017-08-18 11:16:12 +1000

    Logback trying things

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2017 Atlas of Living Australia
+ * All Rights Reserved.
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+
+import ch.qos.logback.core.util.FileSize
 import grails.util.BuildSettings
 import grails.util.Environment
 import org.springframework.boot.logging.logback.ColorConverter
@@ -12,7 +26,6 @@ conversionRule 'wex', WhitespaceThrowableProxyConverter
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         charset = Charset.forName('UTF-8')
-
         pattern =
                 '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
                         '%clr(%5p) ' + // Log level
@@ -33,7 +46,71 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
-root(WARN, ['STDOUT'])
+
+def loggingDir = (System.getProperty('catalina.base') ? System.getProperty('catalina.base') + '/logs' : './logs')
+def appName = 'alerts'
+final TOMCAT_LOG = 'TOMCAT_LOG'
+switch (Environment.current) {
+    case Environment.PRODUCTION:
+        appender(TOMCAT_LOG, RollingFileAppender) {
+            file = "$loggingDir/$appName.log"
+            encoder(PatternLayoutEncoder) {
+                pattern =
+                        '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
+                                '%5p ' + // Log level
+                                '--- [%15.15t] ' + // Thread
+                                '%-40.40logger{39} : ' + // Logger
+                                '%m%n%wex' // Message
+            }
+            rollingPolicy(FixedWindowRollingPolicy) {
+                fileNamePattern = "$loggingDir/$appName.%i.log.gz"
+                minIndex=1
+                maxIndex=4
+            }
+            triggeringPolicy(SizeBasedTriggeringPolicy) {
+                maxFileSize = FileSize.valueOf('10MB')
+            }
+        }
+        root(WARN, ['TOMCAT_LOG'])
+        break
+    case Environment.TEST:
+        appender(TOMCAT_LOG, RollingFileAppender) {
+            file = "$loggingDir/$appName.log"
+            encoder(PatternLayoutEncoder) {
+                pattern =
+                        '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
+                                '%5p ' + // Log level
+                                '--- [%15.15t] ' + // Thread
+                                '%-40.40logger{39} : ' + // Logger
+                                '%m%n%wex' // Message
+            }
+            rollingPolicy(FixedWindowRollingPolicy) {
+                fileNamePattern = "$loggingDir/$appName.%i.log.gz"
+                minIndex=1
+                maxIndex=4
+            }
+            triggeringPolicy(SizeBasedTriggeringPolicy) {
+                maxFileSize = FileSize.valueOf('1MB')
+            }
+        }
+        root(WARN, ['TOMCAT_LOG'])
+        break
+    case Environment.DEVELOPMENT:
+        root(INFO, ['FULL_STACKTRACE','STDOUT'])
+    default:
+        appender(TOMCAT_LOG, ConsoleAppender) {
+            encoder(PatternLayoutEncoder) {
+                pattern = '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
+            }
+        }
+        root(WARN, ['TOMCAT_LOG'])
+        break
+}
+
 [
         (OFF): [],
         (ERROR): [
@@ -57,4 +134,8 @@ root(WARN, ['STDOUT'])
         ],
         (TRACE): [
         ]
-].each { level, names -> names.each { name -> logger(name, level) } }
+].each { level, names ->
+    names.each { name ->
+        logger(name, level)
+    }
+}
