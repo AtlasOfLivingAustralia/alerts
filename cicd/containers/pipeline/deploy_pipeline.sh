ff0be762b483d8b592b39831e8cdb8cf770c98fe
Joe Lipson <joe.lipson@csiro.au>
2024-06-06 09:19:49 +1000

    stackname and var updates

--- cicd/containers/pipeline/deploy_pipeline.sh
+++ cicd/containers/pipeline/deploy_pipeline.sh
@@ -137,7 +137,7 @@ aws cloudformation deploy \
     --region $REGION \
     --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        pAlertsStackName=$ALERTS_STACK_NAME \
+        pBaseStackName=$BASE_STACK_NAME \
         pAutoDeploy=$AUTO_DEPLOY \
         pBootstrapStackName=$BOOTSTRAP_STACK_NAME \
         pBucketsStackName=$BUCKETS_STACK_NAME \
