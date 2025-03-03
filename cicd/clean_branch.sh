d97cef2d75016923716e75426942df6bf68220fd
Joe Lipson <joe.lipson@csiro.au>
2025-03-03 17:08:04 +1100

    prevent clean_branch ending in non alphanumeric

--- cicd/clean_branch.sh
+++ cicd/clean_branch.sh
@@ -3,8 +3,10 @@
 ###
 # Sanitise a git branch name so that it can be used in AWS resource and stack names
 #
-# e.g. feature/Create_new_logo! will be changed to feature-create-new-logo
+# e.g. feature/Create_new_logo! will be changed to create-new-logo
+#
+# some resource names can only end in letters or numbers so we remove any trailing non-alphanumeric characters
 
 SRC_BRANCH=$1
 
-echo $SRC_BRANCH | sed 's/[/_]/-/g' | sed 's/[^a-zA-Z0-9-]//g' | tr '[:upper:]' '[:lower:]' | cut -c 1-30
+echo $SRC_BRANCH | sed 's/^feature\///' | sed 's/[/_]/-/g' | sed 's/[^a-zA-Z0-9-]//g' | tr '[:upper:]' '[:lower:]' | cut -c 1-30 | sed 's/[^a-z0-9]\+$//'
