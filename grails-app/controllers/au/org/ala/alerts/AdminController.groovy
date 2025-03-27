e50402fe7a55d8f7322076bade1f57ded15b1bce
QBai <31238531+qifeng-bai@users.noreply.github.com>
2025-03-28 10:39:58 +1100

    Squashed commit of the following:
    commit a6a3862dcc3f0a013f8eb14145905664eb41104a
    Author: QBai <31238531+qifeng-bai@users.noreply.github.com>
    Date:   Fri Mar 28 10:39:33 2025 +1100
    
        #403 fixed query_path encode in alerts creation

--- grails-app/controllers/au/org/ala/alerts/AdminController.groovy
+++ grails-app/controllers/au/org/ala/alerts/AdminController.groovy
@@ -86,7 +86,7 @@ class AdminController {
     @Transactional
     def fixupBiocacheQueries() {
         def toUpdate = []
-        Query.findAllByQueryPathForUI('/occurrences/search?q=*:*&fq=first_loaded_date:' + '[___DATEPARAM___ TO *]'.encodeAsURL() + '&sort=first_loaded_date&dir=desc').each {
+        Query.findAllByQueryPathForUI('/occurrences/search?q=*:*&fq=first_loaded_date:' + '[___DATEPARAM___  TO *]'.encodeAsURL() + '&sort=first_loaded_date&dir=desc').each {
             it.queryPathForUI = it.queryPath.substring(3)
             toUpdate << it
         }
@@ -94,7 +94,7 @@ class AdminController {
         toUpdate.clear()
 
 
-        Query.findAllByQueryPathForUI('/occurrences/search?q=*:*&fq=user_assertions:*&fq=last_assertion_date:' + '[___DATEPARAM___ TO *]'.encodeAsURL() + '&sort=last_assertion_date&dir=desc').each {
+        Query.findAllByQueryPathForUI('/occurrences/search?q=*:*&fq=user_assertions:*&fq=last_assertion_date:' + '[___DATEPARAM___  TO *]'.encodeAsURL() + '&sort=last_assertion_date&dir=desc').each {
             it.queryPathForUI = it.queryPath.substring(3)
             toUpdate << it
         }
@@ -102,7 +102,7 @@ class AdminController {
         toUpdate.clear()
 
 
-        Query.findAllByQueryPathForUI('/occurrences/search?q=*:*&fq=last_assertion_date:' + '[___DATEPARAM___ TO *]'.encodeAsURL() + '&sort=last_assertion_date&dir=desc').each {
+        Query.findAllByQueryPathForUI('/occurrences/search?q=*:*&fq=last_assertion_date:' + '[___DATEPARAM___  TO *]'.encodeAsURL() + '&sort=last_assertion_date&dir=desc').each {
             it.queryPathForUI = it.queryPath.substring(3)
             toUpdate << it
         }
