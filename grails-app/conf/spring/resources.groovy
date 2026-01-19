84116dc0e4b4dc77dfd88cd1c69cff932394e46d
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-01-19 17:07:24 +1100

    Swapped @SpringBootApplication for @EnableAutoConfiguration
    Removed need for env var for Spring config

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,8 +1,27 @@
 import grails.util.Holders
+import org.springframework.scheduling.quartz.SchedulerFactoryBean
 import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import au.org.ala.alerts.quartz.AutowiringSpringBeanJobFactory
 
 beans = {
     localeResolver(SessionLocaleResolver) {
         defaultLocale= new java.util.Locale(Holders.config.siteDefaultLanguage as String)
+    }
+
+    // 1. Define the custom Quartz JobFactory
+    quartzJobFactory(AutowiringSpringBeanJobFactory)
+
+    // 2. Link it to the Scheduler
+    quartzScheduler(SchedulerFactoryBean) {
+        dataSource = ref('dataSource')
+        transactionManager = ref('transactionManager')
+        jobFactory = ref('quartzJobFactory') // CRITICAL: This enables @Autowired in jobs
+
+        quartzProperties = [
+                'org.quartz.scheduler.instanceName': 'quartzScheduler',
+                'org.quartz.jobStore.class': 'org.springframework.scheduling.quartz.LocalDataSourceJobStore',
+                'org.quartz.jobStore.driverDelegateClass': 'org.quartz.impl.jdbcjobstore.StdJDBCDelegate',
+                'org.quartz.jobStore.tablePrefix': 'QRTZ_'
+        ] as Properties
     }
 }
--- grails-app/init/au/org/ala/alerts/Application.groovy
+++ grails-app/init/au/org/ala/alerts/Application.groovy
@@ -2,9 +2,12 @@ package au.org.ala.alerts
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 
-@SpringBootApplication
+/**
+ * Main Grails configuration class
+ */
+@EnableAutoConfiguration
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
