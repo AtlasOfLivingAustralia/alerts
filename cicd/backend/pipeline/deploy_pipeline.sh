93632885d321694aeb5fc4e60aa62a85ff1e6b78
Hamza Javed <hamza.javed@csiro.au>
2026-07-31 15:48:17 +1000

    Move liquibase into helm chart

--- cicd/backend/pipeline/deploy_pipeline.sh
+++ cicd/backend/pipeline/deploy_pipeline.sh
@@ -141,7 +141,6 @@ aws cloudformation deploy \
         pBootstrapStackName=$BOOTSTRAP_STACK_NAME \
         pBaseStackName=$BASE_STACK_NAME \
         pBucketsStackName=$BUCKETS_STACK_NAME \
-        pVpcStackName=$VPC_STACK_NAME \
         pCleanBranch=$clean_branch \
         pEnvironment=$environment \
         pGitHubBranch=$branch \
